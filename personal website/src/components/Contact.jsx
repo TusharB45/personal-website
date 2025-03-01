0d8e12b4efeaf0a085e86b8d808b182aea8ee98b
Borse Tushar Mukesh <122999869+TusharB45@users.noreply.github.com>
2025-03-01 20:04:45 +0530

    Fixed EmailJS setup, resolved duplicate keys, and improved MapCanvas loading

--- personal website/src/components/Contact.jsx
+++ personal website/src/components/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Tushar Borse",
           from_email: form.email,
           to_email: "tusharborse45@gmail.com",
           message: form.message,
